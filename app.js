a724f1012411639a8a0e7d1240de04751e7a07d5
Alizar2407 <sss240702@gmail.com>
2022-04-18 12:54:55 +0400

    Restructured, now can launch with "npm run dev"

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const PORT = config.get("port") || 5000
+const mongoUri = config.get('mongoUri')
+
+app.use('/api/auth', require('./routes/auth.routes'))
+
+async function start() {
+    try {
+        await mongoose.connect(mongoUri, {
+            useNewUrlParser: true
+        })
+        app.listen(PORT, () => {
+            console.log(`App has started on port ${PORT}...`)
+        })
+    }
+    catch (error) {
+        console.log('Server error', error.message)
+        process.exit(1)
+    }
+}
+
+start()
---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const {Schema} = require("mongoose");
+
+const userSchema = new Schema({
+    login: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    avatar: {
+        type: String,
+        default: ''
+    },
+
+    special_right: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
