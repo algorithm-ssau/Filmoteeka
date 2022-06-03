bd0c83a32276fe6b7c19232f67d244571c60a127
13stulev <somaisnotacoma@gmail.com>
2022-06-03 09:59:30 +0400

    Изменено отображение категорий

--- client/src/components/categories/CategoriesList.js
+++ client/src/components/categories/CategoriesList.js
@@ -3,12 +3,12 @@ import Category from "./Category.js";
 
 function CategoriesList(props) {
   return (
-    <ul>
-      {props.categories.map(category => {
-        return  <Category category={category} key={category.id}/>
+    <ul className="categoriesButtonsWrapper">
+      {props.categories.map((category) => {
+        return <Category category={category} key={category.id} />;
       })}
     </ul>
-  )
+  );
 }
 
 export default CategoriesList;
